132e680e463e54fab4da1f4b50da777acc98d23e
Moritz Mayer-Eckardt <m.mayereckardt@gmai.com>
2024-02-17 20:35:20 +0100

    edited script and style

--- script.js
+++ script.js
@@ -62,8 +62,8 @@ function getBackgroundColor(type) {
 }
 
 function prepareTypesAndColors() {
-    let type1 = currentPokemon.types[0].type.name;
-    let type2 = currentPokemon.types[1] ? currentPokemon.types[1].type.name : null;
+    let type1 = currentPokemon.types['0'].type.name;
+    let type2 = currentPokemon.types['1'] ? currentPokemon.types['1'].type.name : null;
     let color1 = getBackgroundColor(type1);
     let color2 = type2 ? getBackgroundColor(type2) : null;
     return {type1, type2, color1, color2}
@@ -72,7 +72,7 @@ function prepareTypesAndColors() {
 function createSmallCardTemplate(i) {
     let { type1, type2, color1, color2 } = prepareTypesAndColors();
     return /*html*/`
-        <div onclick="openPopup(${i})" class="small-card" style="background-color: ${color1};">
+        <div onclick="openPopup(${i-1})" class="small-card" style="background-color: ${color1};">
             <div class="small-card-top">
                 <h2>${currentPokemon.name.charAt(0).toUpperCase() + currentPokemon.name.slice(1)}</h2> 
                 <div>#${currentPokemon.id.toString().padStart(3, '0')}</div> 
@@ -89,62 +89,132 @@ function createSmallCardTemplate(i) {
 
 function openPopup(i) {
     let popup = document.getElementById('popup');
-    let selectedPokemon = allPokemon[i-1];
+    let selectedPokemon = allPokemon[i];
     popup.classList.remove('d-none');
     renderDetailedCard(i, selectedPokemon);
-    renderPokemonInfo(selectedPokemon);
+}
+
+function getBackGroundColorAndTypesCard(selectedPokemon) {
+    let type1 = selectedPokemon.types['0'].type.name;
+    let type2 = selectedPokemon.types['1'] ? selectedPokemon.types['1'].type.name : null;
+    let color1 = getBackgroundColor(type1);
+    let color2 = getBackgroundColor(type2);
+    return {color1, color2, type1, type2}
 }
 
 function renderDetailedCard(i, selectedPokemon) {
-    let card = document.getElementById('card');
+    let {color1, color2, type1, type2} = getBackGroundColorAndTypesCard(selectedPokemon);
+    let card = document.getElementById('popup');
     card.innerHTML = /*html*/`
-        <div class="card">
-            <div class="pokedex-top">
-                <div class="pokemon-headline">
-                    <h1 id="pokemon-name"></h1>
-                    <div id="pokemon-id"></div>
+        <div id="card">
+                <div class="pokemon-headline" style="color: ${color1}">
+                    <h2 id="pokemon-name">${selectedPokemon.name.charAt(0).toUpperCase() + selectedPokemon.name.slice(1)}</h2>
+                    <div id="pokemon-id">#${selectedPokemon.id.toString().padStart(3, '0')}</div>
                 </div>
-                <img id="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${i}.png" alt="">
-            </div>
-            <div class="info">
-                <div class="height-and-weight">
-                    <div class="height"><span class="color">Height</span><div id="pokemon-height"></div></div>
-                    <div class="weight"><span class="color">Weight</span><div id="pokemon-weight"></div></div>
+                <img id="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${i+1}.png" alt="">
+            <div class="info" style="background-color: ${color1}">
+                <div class="about-stats-moves">
+                    <b onclick="showAbout(${i})" class="category">About</b>
+                    <b onclick="showStats(${i})" class="category">Stats</b>
+                    <b onclick="showMoves(${i})" class="category">Moves</b>
                 </div>
-                <div class="moves-and-index">
-                    <ul id="pokemon-moves">Moves:</ul>
-                    <ul id="pokemon-index">Index:</ul>
+                <div id="about" class="about">
+                    <div class="skill">
+                        <span>Base Experience</span>
+                        <div><b>${selectedPokemon.base_experience}</b></div>
+                    </div>
+                    <div class="skill">
+                        <span>Height</span>
+                        <div><b>${selectedPokemon.height}</b></div>
+                    </div>
+                    <div class="skill">
+                        <span>Weight</span>
+                        <div><b>${selectedPokemon.weight}</b></div>
+                    </div>
+                    <div class="skill">
+                        <span>Abilities</span>
+                        <div class="abilities">
+                            <b>${selectedPokemon.abilities['0'].ability.name}</b>
+                            ${selectedPokemon.abilities.length > 1 ? `<b>${selectedPokemon.abilities['1'].ability.name}</b>` : ''}
+                        </div>
+                    </div>
+                    <div class="types-detailed-card">
+                    <b class="type-detailed-card" style="background-color: ${color1}">${type1}</b>   
+                    ${selectedPokemon.types.length > 1 ? `<b class="type-detailed-card" style="background-color: ${color2}">${type2}</b>` : ''}
+                    </div>
+                    <div class="close-button" onclick="closePopup()">x</div>
                 </div>
             </div>
         </div>
     `;
-    renderPokemonInfo(selectedPokemon);
 }
 
-function renderPokemonInfo(selectedPokemon) {
-    document.getElementById('pokemon-name').innerHTML = selectedPokemon['name'];
-    document.getElementById('pokemon-id').innerHTML += `#${selectedPokemon['id'].toString().padStart(3, '0')}`;
-    document.getElementById('pokemon-height').innerHTML += `<b>${selectedPokemon['height']}</b>`;
-    document.getElementById('pokemon-weight').innerHTML += `<b>${selectedPokemon['weight']}</b>`;
+function closePopup() {
+    let popup = document.getElementById('popup');
+    popup.classList.add('d-none');
 }
 
-
-function renderMoves() {
-    let pokemonMoves = document.getElementById('pokemon-moves')
-    let moves = currentPokemon['moves'];
+function showMoves(i) {
+    let movesContainer = document.getElementById('about');
+    movesContainer.innerHTML = '';
+    movesContainer.classList.remove('about');
+    movesContainer.classList.remove('stats');
+    movesContainer.classList.add('moves');
+    let moves =  allPokemon[i].moves;
     for (let i = 0; i < moves.length; i++) {
-        const move = moves[i]['move']; 
-        pokemonMoves.innerHTML += `<li>${move['name']}</li>`;
+        const move = moves[i].move; 
+        movesContainer.innerHTML += `<b class="move">${move.name}</b>`;
     }
 }
 
-function renderIndices() {
-    let pokemonIndices = document.getElementById('pokemon-index');
-    let indices = currentPokemon['game_indices'];
-    for (let i = 0; i < indices.length; i++) {
-        const index = indices[i]['game_index'];
-        const version = indices[i]['version']['name'];
-        pokemonIndices.innerHTML += `<li>${index} ${version}</li>`;
-    }  
+function showAbout(i) {
+    let aboutContainer = document.getElementById('about');
+    aboutContainer.innerHTML = '';
+    aboutContainer.classList.remove('moves');
+    aboutContainer.classList.remove('stats');
+    aboutContainer.classList.add('about');
+    openPopup(i);
 }
+
+function showStats(i) {
+    let statsContainer = document.getElementById('about');
+    statsContainer.innerHTML = '';
+    statsContainer.classList.remove('moves');
+    statsContainer.classList.remove('about');
+    statsContainer.classList.add('stats');
+    statsContainer.innerHTML = renderStats(i, statsContainer);
+    let stats = allPokemon[i].stats;
+}
+
+function renderStats(i, statsContainer) {
+    let stats = allPokemon[i].stats;
+    return statsContainer.innerHTML += /*html*/`
+    <div class="stats-name">
+        <span>${stats[0].stat.name}</span>
+        <span>${stats[1].stat.name}</span>
+        <span>${stats[2].stat.name}</span>
+        <span>${stats[3].stat.name}</span>
+        <span>${stats[4].stat.name}</span>
+        <span>${stats[5].stat.name}</span>
+    </div>
+    <div class="stats-chart">
+        <div class="bar" style="width: ${stats[0].base_stat}px;"></div>
+        <div class="bar" style="width: ${stats[1].base_stat}px;"></div>
+        <div class="bar" style="width: ${stats[2].base_stat}px;"></div>
+        <div class="bar" style="width: ${stats[3].base_stat}px;"></div>
+        <div class="bar" style="width: ${stats[4].base_stat}px;"></div>
+        <div class="bar" style="width: ${stats[5].base_stat}px;"></div>
+    </div>
+    <div class="stats-value">
+        <b>${stats[0].base_stat}</b>
+        <b>${stats[1].base_stat}</b>
+        <b>${stats[2].base_stat}</b>
+        <b>${stats[3].base_stat}</b>
+        <b>${stats[4].base_stat}</b>
+        <b>${stats[5].base_stat}</b>
+    </div>
+`;
+}
+
+
 
